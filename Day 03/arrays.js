ab82d94ca42b2c8e1908be6e25daa27e8323673e
ammar <ammarraneez@gmail.com>
2022-03-08 22:41:18 +0530

    Add day 03

---
+++ Day 03/arrays.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    const numSet = new Set(nums);
+    const numArray = Array.from(numSet);
+    const maxVal = Math.max(...numArray);
+    numArray.splice(numArray.indexOf(maxVal), 1);
+    return Math.max(...numArray);
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
